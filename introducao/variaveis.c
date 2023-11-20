9f0a8ff8ab64e9982cd6dd8fe2a6c26de2034f41
Lucas Lima <128159917+lucslima96@users.noreply.github.com>
2023-11-20 12:33:02 -0300

    Variaveis
    Declarando variaveis

---
+++ introducao/variaveis.c
@@ -0,0 +1,18 @@
+// Variaveis
+#include <stdio.h>
+
+int main(){ // Inicio do bloco
+
+	//Declarando variaveis
+	int idade;
+
+	//Funcao para escrever algo
+	printf("Qual e a sua idade?\n");
+
+	//Receber dados
+	scanf("%d", &idade);
+
+	//Saída
+	printf("Sua idade é %d", idade);
+	return 0;
+} // Fim do bloco
