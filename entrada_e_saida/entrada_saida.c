7234e82ffb92cc917fb4eb67147e02a8e1722dd2
Lucas Lima <128159917+lucslima96@users.noreply.github.com>
2023-11-29 17:27:17 -0300

    entrada e saida de dados

--- entrada_e_saida/entrada_saida.c
+++ entrada_e_saida/entrada_saida.c
@@ -7,7 +7,8 @@ int main(){
 	//fopen(nome do arquivo, forma de abertura do arquivo)
 	//w - abrir o arquivo para escrita (se o arquivo ja existir sera sobrescrito com um novo zerado
 	//r - abrir o arquivo para leitura ( nao podemos escrever no arquivo)
-	//wa - abrir o arquivo para adicao de conteudo( se o arquivo ja existir, o conteudo sera adicionado nas linhas abaixo)
+	//a - anexa novos dados
+	//
 
 	arq = fopen("arquivo.txt", "w");
 	//sempre que a gente terminar a manipulacao de um arquivo devemos fecha-lo
