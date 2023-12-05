d9e0d0b55e8b1b1962c12ba6409156b67c053528
Lucas Lima <128159917+lucslima96@users.noreply.github.com>
2023-12-05 12:40:03 -0300

    stdlib

---
+++ bibliotecas/stdlib.h.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+//atof() //converte string para double
+//atoi() //converte string para inteiro
+//atol() // converte string para inteiro longo
+
+//Geracao de sequencia pseudo-aleatoria
+
+//rand() //gera numero aleatorio
+//srand() // inicializa o gerador de numeros aleatorios
+
+//Gerenciamento de memoria dinamica
+
+//maloc()
+//callod()
+//free()
+//realoc()
+
+//Pesquisa e ordenacao
+
+//qsort() //ordena os elementos de um array
+
+//Aritmetica de inteiros
+
+//abs() // valor absoluto
+//div() // divisao inteira
