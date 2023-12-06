d7e852df5e5f85e00ab5f5cddf3b3d1961b7409f
Lucas Lima <128159917+lucslima96@users.noreply.github.com>
2023-12-06 01:08:26 -0300

    math.h

---
+++ bibliotecas/math.h.c
@@ -0,0 +1,41 @@
+#include <math.h>
+
+/*Funcoes Matematicas
+ *
+ * a)Clicar com o botao direito do mouse no projeto_c
+ *
+ * b)It em C/C++ Build -> Settings -> Libraries
+ *
+ * c)Clicar no + e add 'm'
+ *
+ * d)Aplicar e fechar
+ *
+ * Funcoes Trigonometricas
+ *
+ * cos() -> Calcula o cosseno de um angulo em radianos
+ * sin() -> Calcula o seno de um angulo em radianos
+ * tang() -> Calcula a tangente de um angulo em radianos
+ *
+ * Funcoes Hiperbolicas
+ *
+ * cosh()-> Calcula o cosseno hiperbolico de um angulo em radianos
+ * sinh()-> Calcula o seno hiperbolico de um angulo em radianos
+ * tanh()-> Calcula a tangente hiperbolica de um angulo em radianos
+ *
+ * Funcoes Exponenciais e Logaritmicas
+ *
+ * exp()-> Funcao exponencial
+ * log()-> Logaritmo natural
+ * log10()-> Logaritmo na base 10
+ *
+ * Funcoes de potencia
+ *
+ * pow(base, expoente)-> retorna a base elevada ao expoente
+ * sqrt()-> Raiz quadrada de um numero
+ *
+ * Funcoes de arredondamento, valor absoluto e outras
+ *
+ * ceil()-> arredonda pra cima
+ * floor()-> arredonda pra baixo
+ *
+ */
