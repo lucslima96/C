ccd5a3d4937926158656872cfa434e970f338b1e
Lucas Lima <128159917+lucslima96@users.noreply.github.com>
2023-12-06 01:25:40 -0300

    ctype.h

---
+++ bibliotecas/ctype.h.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+/*
+ * isalnum()-> verifica se o caractere é alfanumerico(abc123)
+ * isalpha(c) -> Verifica se o caractere c é uma letra do alfabeto
+ * isdigit(c) -> Verifica se o caractere c é um dígito numérico
+ * isalnum(c) -> Verifica se o caractere c é alfanumérico (letra ou dígito)
+ * isupper(c) -> Verifica se o caractere c é uma letra maiúscula
+ * islower(c) -> Verifica se o caractere c é uma letra minúscula
+ * isspace(c) -> Verifica se o caractere c é um espaço em branco
+ * isprint(c) -> Verifica se o caractere c é imprimível
+ * ispunct(c) -> Verifica se o caractere c é um caractere de pontuação
+ * isxdigit(c) -> Verifica se o caractere c é um dígito hexadecimal
+ * isupper(c) -> verifica se é maiuscula
+ * isspace(c) -> verifica se é um espaco
+ *
+ * Funções de Conversão de Caracteres
+ *
+ * tolower(c) -> Converte o caractere c para minúsculo, se for uma letra
+ * toupper(c) -> Converte o caractere c para maiúsculo, se for uma letra
+ */
